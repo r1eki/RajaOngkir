71637f87885857b3fe4fb2fad67a14c9f89d1bcb
r1eki <muhammadrakayusrifan@gmail.com>
2018-09-14 15:13:10 +0700

    Update 2018_09_14_074144_create_province_table.php

--- database/migrations/2018_09_14_074144_create_province_table.php
+++ database/migrations/2018_09_14_074144_create_province_table.php
@@ -13,7 +13,7 @@ class CreateProvinceTable extends Migration
      */
     public function up()
     {
-        Schema::create('province', function (Blueprint $table) {
+        Schema::create('provinsi', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('province_id');
             $table->string('province');
@@ -28,6 +28,6 @@ class CreateProvinceTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('province');
+        Schema::dropIfExists('provinsi');
     }
 }
