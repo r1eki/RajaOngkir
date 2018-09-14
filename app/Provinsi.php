cdf92297565971c4c6551aa3ee7057d11f5081c5
r1eki <muhammadrakayusrifan@gmail.com>
2018-09-14 14:55:24 +0700

    Sprint1

---
+++ app/Provinsi.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace App;
+
+class Provinsi extends Api {
+	protected $method = "province";
+}
---
+++ database/migrations/2018_09_14_073632_create_city_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCityTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        chema::create('city', function (Blueprint $table) {
+            $table->increments('city_id');
+            $table->integer('province_id');
+            $table->string('province');
+            $table->string('type');
+            $table->string('city_name');
+            $table->string('postal_code');
+            $table->timestamp('created_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('city');
+    }
+}
